dff73e9016a9c7aa9e6e66980f1e69afda54606a
陈刚 <cg@chengangdeMacBook-Pro.local>
2015-03-04 22:10:31 +0800

    Initial Commit

---
+++ sofun/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  sofun
+//
+//  Created by 陈刚 on 15/3/4.
+//  Copyright (c) 2015年 cg. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
